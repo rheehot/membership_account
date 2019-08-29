e8d157f2fb1c056aae718bc787e66d4129ad1fb1
조애리 <joaeli@joaeliui-MacBookPro.local>
2019-08-29 13:13:36 +0900

    회원가입 아이디 유효성체크 validareID 구현

---
+++ join/join.js
@@ -0,0 +1,30 @@
+const showMsg = {
+    init(){
+        document.querySelector('.form').insertAdjacentHTML('beforeend',);
+        this.registerEvent();
+    },
+    registerCheckEvent(){
+        const target = document.querySelector('')
+    }
+}
+
+const validateID = {
+    regExpId: /^[0-9a-z_-]{5,20}$/,
+    msg:{
+        taken:'이미 사용중인 아이디입니다.',
+        wrong:'5-20자의 영문 소문자, 숫자와 특수기호(_)(-) 만 사용 가능합니다.',
+        good: '사용가능한 아이디입니다.'
+    },
+    validate(id){
+        if(!this.regExpId.test(id)){ return this.msg.wrong; }
+        else if(this.checkDuplication(id)){ return this.msg.taken; }
+        else return this.msg.good;
+    },
+    checkDuplication(id){
+        const data = ['boostcamp','boost'];
+        return data.includes(id);
+    }
+}
+const validatePW = {
+    
+}
